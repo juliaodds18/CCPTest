0328f5ddef8381c9c67560615adb835e9b05d39a
juliaodds18 <juliaodds18@gmail.com>
2019-06-30 13:21:08 +0000

    First iteration, check if point falls within the area of rect (no dist involved yet)

--- DistanceFromARectangle.py
+++ DistanceFromARectangle.py
@@ -16,6 +16,24 @@ def is_point_within_dist_of_rect(rect=Rectangle(), point=Point(), dist=0.0):
 	# TO DO: Check if point falls within dist of rec 
 	# First iteration: Check if point is within the area of rect 
 
+	# Find the location of the bottom right point 
+	bottom_right = Point(rect.bottom_left.x + rect.width, rect.bottom_left.y)
+	# Find the location of the top left point 
+	top_left = Point(rect.bottom_left.x, rect.bottom_left.y + rect.height)
+
+	
+	# Check if the point falls within the rectangle's x-scale
+	if (point.x >= rect.bottom_left.x and point.x <= bottom_right.x):
+		# Check if the point falls within the rectangle's y-scale
+		if (point.y >= rect.bottom_left.y and point.y <= top_left.y):
+			# The point falls within the area of the rectangle, return true
+			return True	
 	
 
-	pass
+	return False
+
+
+
+rect = Rectangle(0.0, 0.0, 1.0, 1.0)
+point = Point(1.0, 1.0)
+is_point_within_dist_of_rect(rect, point)
