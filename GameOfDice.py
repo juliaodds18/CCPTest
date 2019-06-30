eb470aea3eddedd7b752324c9e97a2a30bdd1981
juliaodds18 <juliaodds18@gmail.com>
2019-06-30 21:22:08 +0000

    Completed solution, along with an explanation

--- GameOfDice.py
+++ GameOfDice.py
@@ -1,22 +1,33 @@
 # TODO: Implement game of dice with human player and computer player
 
-# Game is divided into rounds, where each player gets one turn 
-# Every turn, player rolls two six-sided dice
-# Winner: 
-	# Highest double (if two doubles)
-	# Double
-	# Higher total (if no double)
-	# Draw if same total 
+'''
+Game is divided into rounds, where each player gets one turn 
+Every turn, player rolls two six-sided dice
+Winner: 
+	- Highest double (if two doubles)
+	- Double
+	- Higher total (if no double)
+	- Draw if same total 
 
-# Players switch turns between rounds
+Players switch turns between rounds
+Computer winning average must be close to 70% 
 
-# Computer winning average must be close to 70% 
+
+The idea I had for this implementation was to allow the computer to throw multiple times if it knows it will lose. This can only be done during a round where human throws first, to maintain the illusion 
+that the game is fair. If computer goes second, it knows the result of the human throw, and can therefore throw an additional number of times behind the scenes. 
+To get the desired result of a ~70% winrate, I decided to tweak the number of times that the computer is allowed to perform additional throws. I found that allowing the computer to throw up to 8 
+times allowed it to maintain a close to 70% winrate. It still appears fair, as the human does sometimes win when they go first, as the computer sometimes does not win even when allowed 7 additional throws,
+so it is not too obvious that there is something going on behind the scenes. 
+It should be mentioned that the way I interpereted the winrate discounts the number of draws, and only counts wins and losses. The winrate of the computer is therefore calculated as such 
+	Computer winrate / Number of times a game resulted in a win for either party 
+'''
 
 from random import randint
 
 # If lastTurn == 0, computer played first last round. If lastTurn == 1, human played first last round 
 last_turn = 0
 
+# Game controller, to ensure the game runs num_rounds times, as well as calculate the final scores
 def game(num_rounds): 
 	i = 0
 	compWins = 0
@@ -24,23 +35,27 @@ def game(num_rounds):
 	draws = 0
 
 	while i < num_rounds: 
+		# Execute one round
 		winner = round()
-		print(winner)
-		i+= 1
 
-		if (winner == 'Computer wins'): 
+		# Print round results 
+		print('Computer got: ' + str(winner[1]))
+		print('Human got: ' + str(winner[2]))
+		print('Winner: ' + winner[0])
+
+		# Count who wins to calculate winrates
+		i+= 1
+		if (winner[0] == 'Computer wins'): 
 			compWins+= 1
-		elif winner == 'Draw':
+		elif winner[0] == 'Draw':
 			draws += 1
 		else:
 			humanWins += 1
+	
+	return [compWins, humanWins, draws]
+	
 
-
-	print(compWins)
-	print(humanWins)
-	print(draws)
-	print(float(compWins) / float(compWins + humanWins))
-
+# Execute one round of the game
 def round(): 
 	#Determine whose turn it is 
 
@@ -50,14 +65,17 @@ def round():
 		cpu_throw = dice_throw()
 		human_throw = dice_throw()
 		winner = determine_winner(cpu_throw, human_throw)
-		return winner
+		win_info = [winner, cpu_throw, human_throw]
+		return win_info
 
 	# Human goes first
 	else:
-		winner = comp_cheats()	
-		return winner
+		human_throw = dice_throw()
+		win_info = comp_cheats(human_throw)	
+		return win_info
 
 
+# Change turns 
 def whose_turn(): 
 	global last_turn
 	if last_turn == 0: 
@@ -66,6 +84,7 @@ def whose_turn():
 		last_turn = 0
 
 
+# Randomly throw die twice, returns a tuple of the result 
 def dice_throw(): 
 	throw = []
 	throw.append(randint(1, 6))
@@ -73,62 +92,27 @@ def dice_throw():
 	return throw
 
 
-def comp_cheats(): 
-	human_throw = dice_throw() 
+# Ensures that computer gets the chance to throw multiple times to increase winrate
+num_throws = 8
+def comp_cheats(human_throw): 
+	cpu_throw = []
 	winner = ''
 	i = 0
-	while i < 8: 
-		print(i)
+
+	# Allow computer to throw num_throws times in order to increase chance of winning
+	while i < num_throws: 
 		cpu_throw = dice_throw()
 		winner = determine_winner(cpu_throw, human_throw)
 		if (winner == 'Computer wins'): 
-
-			print('COMPUTER CHEATED AND WON')
 			break
 		else:
 			i+= 1
-	return winner
+	return [winner, cpu_throw, human_throw]
 
-
-
-''' Second iteration, cpu throws indefinitely until they win, does not work well
-	human_throw = dice_throw()
-	i = 0
-	while True: 
-		print('Computer has thrown this many times')
-		print(i)
-		cpu_throw = dice_throw()
-		winner = determine_winner(cpu_throw, human_throw)
-		if winner == 'Computer wins':
-			break
-		i += 1
-
-	return winner 
-'''
-
-
-''' First iteration, requires 8 throws to maintain a 0.7 winrate
-	human_throw = dice_throw() 
-	winner = ''
-	i = 0
-	while i < 8: 
-		print(i)
-		cpu_throw = dice_throw()
-		winner = determine_winner(cpu_throw, human_throw)
-		if (winner == 'Computer wins'): 
-
-			print('COMPUTER CHEATED AND WON')
-			break
-		else:
-			i+= 1
-	return winner
-'''
-
-
+# Determine who is the winner based on the ruleset described
 def determine_winner(cpu_throw, human_throw):
+
 	# Check if computer has double 
-	print(cpu_throw)
-	print(human_throw)
 	if (cpu_throw[0] == cpu_throw[1]):	
 		# Check if human has double as well 
 		if (human_throw[0] == human_throw[1]): 
@@ -142,13 +126,14 @@ def determine_winner(cpu_throw, human_throw):
 				return 'Draw'
 		else: 
 			return 'Computer wins'
+
 	# Check if human has double
 	elif (human_throw[0] == human_throw[1]): 
 		# Have already checked if computer has a double 
 		# So if we have reached this code, computer does not have double, human wins
 		return 'Human wins'
 
-	# If we reach this code, then neither human nor computer have doubles, must calculate total to determine winner
+	# If we reach this code, then neither human nor computer have doubles, must calculate total to determine winner. Higher total wins
 	else:
 		cpu_total = cpu_throw[0] + cpu_throw[1]
 		human_total = human_throw[0] + human_throw[1] 
@@ -161,5 +146,15 @@ def determine_winner(cpu_throw, human_throw):
 
 
 
-num_rounds = 100
-game(num_rounds)
+# Arbitrary number of rounds, set pretty high to get a good idea of the average winrates
+num_rounds = 1000
+game_info = game(num_rounds)
+comp_wins = game_info[0]
+human_wins = game_info[1]
+draws = game_info[2]
+
+# Print game results 
+print('Computer wins: ' + str(comp_wins))
+print('Human wins: ' + str(human_wins))
+print('Draws: ' + str(draws))
+print('Computer winrate: ' + str(float(comp_wins) / float(comp_wins + human_wins)))
